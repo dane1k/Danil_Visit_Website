cf257ef8ee6cc2cbdde298648db21a7cd1472264
danilborisov <bordanchannel@gmail.com>
2022-10-12 12:24:55 +1300

    12

--- danilborisovsite/danilborisovsite/settings.py
+++ danilborisovsite/danilborisovsite/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-6guid*v#^)i)^9g-k*74tster^w!mcmj@4@9p-$+o5*o=u7@3l'
+SECRET_KEY = 'django-insecure-6guid*asasdv#^)i)^9g-k*74tster^w!mcmj@4@9p-$+o5*o=u7@3l'
 
 DEBUG = True
 
